55d374d266fa233d40d79dcc5175f34fff74eec9
Samuel Ferreira <samurodrigo@hotmail.com>
2019-04-24 05:04:04 -0300

    Primeiro commit

---
+++ app/controllers/cadastro.js
@@ -0,0 +1,20 @@
+module.exports.cadastro = function(application, req, res){
+    res.render("cadastro", {erros: null, dadosForm: {}})
+}
+
+module.exports.cadastrar = function(application, req, res){
+    var dadosForm = req.body;
+
+    req.assert("nome", "O campo nome é obrigatório.").notEmpty()
+    req.assert("usuario", "O campo usuário é obrigatório").notEmpty()
+    req.assert("senha", "O campo senha é obrigatório").notEmpty()
+    req.assert("casa", "Selecione uma casa").notEmpty()
+
+    var erros = req.validationErrors()
+
+    if(erros){
+        res.render("cadastro", {erros: erros, dadosForm: dadosForm})
+        return
+    }
+    res.send("Podemos cadastrar")
+}
