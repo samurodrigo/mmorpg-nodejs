87e5b27375382222dbf20a57c262b86598a52f98
Samuel Ferreira <samuel.ferreira@valtech.com>
2019-04-24 17:42:14 -0300

    Adicionada bandeira pelo casa do usuario

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,6 +1,6 @@
 module.exports.jogo = function(application, req, res){
     if(req.session.autorizado){
-        res.render("jogo")
+        res.render("jogo", {img_casa: req.session.casa})
         return
     }
     res.send("Usuário precisa fazer login")
